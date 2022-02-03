e52d8b2c32962587e5cfb8b0868e70338cf03921
Onexerox <86905093+Onexerox@users.noreply.github.com>
2022-02-03 16:07:05 +0300

    Added primitive enemy spawner manager class. Added  ChunkEnemyTypeContainer class which contains specified enemy types for this chunk.

---
+++ Assets/Scripts/Enemy/ChunkEnemyTypeContainer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Core;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class ChunkEnemyTypeContainer : MonoBehaviour
+    {
+        [SerializeField] private List<EnemyPrefabInfo> prefabInfoList;
+
+        public UniqueId GetPrefabByProbability()
+        {
+            return ObjectPool.GetPrefabByProbability(prefabInfoList);
+        }
+
+        private void OnEnable()
+        {
+            EnemySpawnerManager.Inst.AddContainerToList(this);
+        }
+        private void OnDisable()
+        {
+            EnemySpawnerManager.Inst.RemoveContainerFromList(this);
+        }
+    }
+}
