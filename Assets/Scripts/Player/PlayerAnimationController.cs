334a5e4baa730dc82aa24007ab0d31595e1f4be1
Onexerox <86905093+Onexerox@users.noreply.github.com>
2022-03-07 10:09:40 +0300

    Added animations. Started working with own animations of character

--- Assets/Scripts/Player/PlayerAnimationController.cs
+++ Assets/Scripts/Player/PlayerAnimationController.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class PlayerAnimationController : MonoBehaviour
+namespace Player
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerAnimationController : MonoBehaviour
     {
-        
-    }
+        private Animator _animator;
+        private Rigidbody _rb;
+        private static readonly int VelocityX = Animator.StringToHash("VelocityX");
+        private static readonly int VelocityZ = Animator.StringToHash("VelocityZ");
 
-    // Update is called once per frame
-    void Update()
-    {
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+            _animator = GetComponentInChildren<Animator>();
+        }
         
+        private void Update()
+        {
+            var angle = Vector3.SignedAngle(Vector3.forward, transform.forward, Vector3.up);
+            var velocity = Quaternion.Euler(0, -angle, 0) *_rb.velocity;
+            _animator.SetFloat(VelocityX, velocity.x);
+            _animator.SetFloat(VelocityZ, velocity.z );
+        }
+
+        /*private void OnDrawGizmos()
+        {
+            var angle = Vector3.SignedAngle(Vector3.forward, transform.forward, Vector3.up);
+            var velocity = Quaternion.Euler(0, -angle, 0) *_rb.velocity.normalized;
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, transform.position + transform.forward * 5f);
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position,transform.position + velocity * 5f);
+        }*/
     }
 }
